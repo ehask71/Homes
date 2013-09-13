f2f83ee3d8f0c4cff59ea738c9a4cf553735672d
Eric Haskins <ehask71@gmail.com>
2013-09-13 00:10:30 -0400

    bPopup

---
+++ app/Config/database.php
@@ -0,0 +1,16 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'ehask_amer',
+		'password' => '12131213',
+		'database' => 'ehask_rei',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+
+}
+?>
