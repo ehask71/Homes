164617dea73ab140b341d1f85b56bf4d51198e72
Eric Haskins <ehask71@gmail.com>
2013-09-17 01:24:14 -0400

    RoundRobin

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -7,7 +7,25 @@ class AppController extends Controller {
     
     public $viewClass = 'Theme';
     public $theme = 'default';
-    public $components = array('DebugKit.Toolbar');
+    public $components = array(
+        'DebugKit.Toolbar',
+        'Session',
+        'Auth' => array(
+            'authorize' => array('Tiny'),
+            'authenticate' => array(
+                'all' => array('userModel' => 'Account'),
+                'Form' => array(
+                    'fields' => array('username' => 'email', 'password' => 'password'),
+                    'scope' => array(
+                        'Account.is_active' => 'yes'
+                    ),
+                    'recursive' => 1,
+            )),
+            'flash' => array('key' => 'auth', 'element' => 'alerts/error'),
+            'loginRedirect' => array('controller' => 'home', 'action' => 'index'),
+            'logoutRedirect' => array('controller' => 'account', 'action' => 'login'),
+            'loginAction' => '/login',
+        ));
     
     
 }
