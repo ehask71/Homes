72e8a792f152b179295629b555bce3e649eaaba8
Eric Haskins <ehask71@gmail.com>
2013-09-17 00:33:41 -0400

    RoundRobin

---
+++ app/Model/RoleUser.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * CakePHP RoleUser
+ * @author Eric
+ */
+App::uses('AppModel', 'Model');
+
+class RoleUser extends AppModel {
+    
+    public $primaryKey = 'id';
+    
+    public $useTable = 'roles_users';
+    
+    public $defaultRoleId = 2;
+    
+    public function addUserSite($userid,$roleid=0){
+	$data = array('RoleUser'=> array(
+	    'user_id' => (int)$userid,
+	    'role_id' => ($roleid != 0)?$roleid:$this->defaultRoleId
+	));
+	if($this->save($data)){
+	    return true;
+	}
+	return false;
+    }
+
+}
+
