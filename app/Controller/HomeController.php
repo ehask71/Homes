23e60745573f1ea3f0d7aaa3265ecc63e1b1d4d3
ehask71 <ehask71@gmail.com>
2013-09-25 01:22:06 -0400

    Front Form

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -7,6 +7,8 @@ App::uses('AppController', 'Controller');
 
 class HomeController extends AppController {
     
+    public $uses = array('ZipCodes');
+    
     public function beforeFilter() {
 	parent::beforeFilter();
 	$this->Auth->allow();
@@ -16,11 +18,26 @@ class HomeController extends AppController {
         
     }
     
-    public function sell($slug=null){
-        $this->set('slug',$slug);
+    public function sell(){
+        if($this->request->is('post')){
+	    // From the Index
+	    if(isset($this->request->params['fzip']) && isset($this->request->params['fname']) && isset($this->request->params['femail'])){
+		$data = array(
+		    'name' => $this->request->params['fname'],
+		    'email' => $this->request->params['femail'],
+		    'phone' => $this->request->params['fphone'],
+		    'zip' => $this->request->params['fzip'],
+		    'address' => $this->request->params['faddress']
+		);
+		$this->request->data = $data;
+	    } else {
+		
+	    }
+	}
     }
     
     public function buy(){
+	// Check for Slug Params
         if(isset($this->request->params['county']) && isset($this->request->params['state'])){
             $this->set('slug',$this->request->params['county'].'-'.$this->request->params['state']);
         }
