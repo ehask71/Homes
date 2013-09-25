2599e7d9ee8d020698125fbd36d7f91dcbd0774a
Eric Haskins <ehask71@gmail.com>
2013-09-25 16:32:13 -0400

    forgetpwd

---
+++ app/Controller/SeoController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * CakePHP SeoController
+ * @author Eric
+ */
+App::uses('AppController', 'Controller');
+
+class SeoController extends AppController {
+    
+    public $name = 'Seo';
+    
+    public function index(){
+        
+    }
+    
+    public function sitemap(){
+	
+    }
+    
+}
