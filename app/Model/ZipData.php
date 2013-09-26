2c1fec823776f1315271d13cdecadb20ba3a40b4
Eric Haskins <ehask71@gmail.com>
2013-09-26 18:44:46 -0400

    AccountController

---
+++ app/Model/ZipData.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * CakePHP ZipData
+ * @author Eric
+ */
+App::uses('AppModel', 'Model');
+
+class ZipData extends AppModel {
+
+    public $primaryKey = 'id';
+    public $useTable = 'zip_data';
+    public $hasMany = array(
+        'ZipCodes' => array(
+            'className' => 'ZipCodes',
+            'foreignKey' => 'fips'
+            )
+    );
+    
+    
+
+}
+
