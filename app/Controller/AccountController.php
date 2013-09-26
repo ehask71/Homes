fe014b2564cc7e173f8a743384b49782c1dfd832
Eric Haskins <ehask71@gmail.com>
2013-09-25 21:57:47 -0400

    Style

--- app/Controller/AccountController.php
+++ app/Controller/AccountController.php
@@ -17,5 +17,22 @@ class AccountController extends AppController {
         
     }
     
+    public function register(){
+        
+    }
+
+    public function login() {
+	if ($this->request->is('post')) {
+	    if ($this->Auth->login()) {
+		$this->redirect($this->Auth->redirect());
+	    } else {
+		$this->Session->setFlash('Your username or password was incorrect.');
+	    }
+	}
+    }
+    
+    public function logout() {
+        
+    }
 }
 
