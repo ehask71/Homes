87f9ac9994280e02d7b24f83c2d86a31eb07111a
Eric Haskins <ehask71@gmail.com>
2013-09-27 00:47:28 -0400

    sell.ctp

--- app/Model/ZipData.php
+++ app/Model/ZipData.php
@@ -10,10 +10,10 @@ class ZipData extends AppModel {
 
     public $primaryKey = 'id';
     public $useTable = 'zip_data';
-    public $belongsTo = array(
+    public $hasMany = array(
         'ZipCodes' => array(
             'className' => 'ZipCodes',
-            'foreignKey' => 'fips'
+            'foreignKey' => 'zd_id'
         )
     );
     
