bce2f4c0facb39319fdeeb15151728578210c305
Eric Haskins <ehask71@gmail.com>
2013-09-26 21:28:52 -0400

    Lead Model

---
+++ app/Model/Lead.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * CakePHP Lead
+ * @author Eric
+ */
+App::uses('AppModel', 'Model');
+
+class Lead extends AppModel {
+
+    public $primaryKey = 'id';
+
+    public function validateLead() {
+        $validate1 = array(
+            'firstname' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter players first name')
+            ),
+            'lastname' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter players last name')
+            ),
+            'email' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter an email')
+            ),
+            'phone' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter a phone number')
+            ),
+            'address' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter the address')
+            ),
+            'city' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter the city')
+            ),
+            'state' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please select the state')
+            ),
+            'zip' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter the zip code')
+            ),
+            'bathrooms' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please select bathrooms')
+            ),
+            'bedrooms' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please select bedrooms')
+            ),
+            'propertytype' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please select the property type')
+            ),
+            'askingprice' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please select the asking price range')
+            ),
+            'reason' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter your reason for selling')
+            ),
+        );
+
+        $this->validate = $validate1;
+        return $this->validates();
+    }
+
+}
+
