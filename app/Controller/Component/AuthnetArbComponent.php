5f38a95d133625f0031273d3c399d1b9802b9e22
Eric Haskins <ehask71@gmail.com>
2013-09-28 23:43:15 -0400

    Auth.net Arb

---
+++ app/Controller/Component/AuthnetArbComponent.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * CakePHP AuthnetArbComponent
+ * @author Eric
+ */
+class AuthnetArbComponent extends Component {
+
+    public $components = array();
+
+    public function initialize($controller) {
+        
+    }
+
+    public function startup($controller) {
+        
+    }
+
+    public function beforeRender($controller) {
+        
+    }
+
+    public function shutDown($controller) {
+        
+    }
+
+    public function beforeRedirect($controller, $url, $status = null, $exit = true) {
+        
+    }
+
+}
