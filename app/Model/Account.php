c16f46ec23d237d53fd36105b683a8809591718e
Eric Haskins <ehask71@gmail.com>
2013-10-02 02:06:43 -0400

    Account view Files

--- app/Model/Account.php
+++ app/Model/Account.php
@@ -7,5 +7,22 @@ App::uses('AppModel', 'Model');
 
 class Account extends AppModel {
     public $primaryKey = 'id';
+    
+    public $hasAndBelongsToMany = array(
+	'Role' => array(
+	    'className' => 'Role',
+	    'joinTable' => 'roles_users',
+	    'foreignKey' => 'user_id',
+	    'assosciationForeignKey' => 'role_id',
+	    'unique' => 'keepExisting'
+	)
+    );
+    public $hasMany = array(
+	'RoleUser' => array(
+	    'className' => 'RoleUser',
+	    'foreignKey' => 'user_id',
+	    'dependant' => true
+	)
+    );
 }
 
