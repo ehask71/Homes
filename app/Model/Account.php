d39e83eeb67282a65d38e01d18400cfbd3e38ca4
Eric Haskins <ehask71@gmail.com>
2013-10-02 02:07:50 -0400

    Account view Files

--- app/Model/Account.php
+++ app/Model/Account.php
@@ -24,5 +24,119 @@ class Account extends AppModel {
 	    'dependant' => true
 	)
     );
+    
+    public function beforeSave($options = array()) {
+	if (isset($this->data[$this->alias]['password'])) {
+	    $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+	}
+	return true;
+    }
+    
+    public function accountValidate() {
+	$validate1 = array(
+	    'firstname' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please enter your First Name')
+	    ),
+	    'lastname' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please enter your Last Name')
+	    ),
+            'address' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please Enter your Address')
+	    ),
+            'city' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please Enter your City')
+	    ),
+            'state' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please Enter your State')
+	    ),
+	    'zip' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please Enter your Zip/Postal Code')
+	    ),
+	    'country' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please Select Your Country'
+		)
+	    ),
+            'phone' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please enter Your Contact Number'
+		)
+	    ),
+	    'gender' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please Select Your Gender'
+		)
+	    ),
+	    'email' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please Enter Your Email'
+		),
+		'validEmailRule' => array(
+		    'rule' => 'email',
+		    'message' => 'Invalid email address'
+		),
+		'uniqueEmailRule' => array(
+		    'rule' => 'isUnique',
+		    'message' => 'Email already registered'
+		)
+	    ),
+	    'password' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please Enter Your Password'
+		),
+		'passwordequal' => array(
+		    'rule' => 'checkpasswords',
+		    'message' => 'Passwords dont match'
+		)
+	    ),
+	    'confirm_password' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please Confirm Password'
+		)
+	    ),
+	    'agever' => array(
+		'notEmpty' => array(
+		    'rule' => array('comparison', '!=', 0),
+		    'required' => true,
+		    'message' => 'We require you to be over 13 to register without a Parent.'
+		)
+	    ),
+	    'agreeterms' => array(
+		'notEmpty' => array(
+		    'rule' => array('comparison', '!=', 0),
+		    'required' => true,
+		    'message' => 'Please Agree to the Terms if you want to proceed.'
+		)
+	    ),
+	);
+
+	$this->validate = $validate1;
+	return $this->validates();
+    }
+
+    function checkpasswords() {
+	if (strcmp($this->data['Account']['password'], $this->data['Account']['confirm_password']) == 0) {
+	    return true;
+	}
+	return false;
+    }
 }
 
