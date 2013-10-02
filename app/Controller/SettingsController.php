d14ff36319e8230ed588362884bc7dd65cd80417
Eric Haskins <ehask71@gmail.com>
2013-10-02 01:29:21 -0400

    Auth.net Arb

--- app/Controller/SettingsController.php
+++ app/Controller/SettingsController.php
@@ -17,5 +17,29 @@ class SettingsController extends AppController {
         
     }
     
+    public function admin_pricing(){
+        if ($this->request->is('post')) {
+            // We need to Update Pricing
+            /*
+             0-49,999	 $300 per month
+             50,000-249,999	 $500 per month
+             250,000-499,999	 $750 per month
+             500,000-749,999	 $1000 per month
+             750,000-999,999	 $1250 per month
+             1m-1.99m	 $1500 per month
+             2m-2.99m	 $2000 per month
+             3m+	 $2500 per month
+             */
+            $county = $this->ZipData->find('all',array(
+                'conditions' => array(
+                    'ZipData.state !=' => ''
+                )
+            ));
+            
+            foreach ($county AS $val){
+                echo $val['ZipData']['county'].' '.$val['ZipData']['PST045212'].'<br>';
+            }
+        }
+    }
 }
 
