e80b7649bbacfede88a630745cc1aa2c0cee25de
Eric Haskins <ehask71@gmail.com>
2013-10-02 11:46:12 -0400

    AuthNet Config Vars

--- app/Controller/SettingsController.php
+++ app/Controller/SettingsController.php
@@ -12,7 +12,7 @@ class SettingsController extends AppController {
     
     public function beforeFilter() {
 	parent::beforeFilter();
-        $this->Auth->allow();
+        $this->Auth->allow('index');
     }
     
     public function index(){
