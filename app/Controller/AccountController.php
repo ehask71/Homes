7921038cb08cd65e566ee253347c12ae3e5a4b6f
Eric Haskins <ehask71@gmail.com>
2013-10-04 02:20:22 -0400

    AuthNetXML Component

--- app/Controller/AccountController.php
+++ app/Controller/AccountController.php
@@ -51,6 +51,7 @@ class AccountController extends AppController {
                     
 		    $cimresponse = $this->AuthNetXml->customer_profile_request($data);
                     if(!$cimresponse->isError()){
+                        CakeLog::write('debug','CIM success');
                         $update['id'] = $userid;
                         $update['authnet_profile'] = $cimresponse->customerProfileId;
                         $this->Account->save($update);
