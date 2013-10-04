f04869ab482554d9965380359fb04ba872d89433
Eric Haskins <ehask71@gmail.com>
2013-10-04 01:01:43 -0400

    AuthNetXML

---
+++ app/Controller/Component/AuthNetXmlComponent.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * CakePHP AuthNetXmlComponent
+ * @author Eric
+ */
+App::import('Vendor', 'Facebook', array('file' => 'Authnet' . DS . 'AuthNetXML.class.php'));
+class AuthNetXmlComponent extends Component {
+
+    public $components = array();
+
+    public function __construct(\ComponentCollection $collection, $settings = array()) {
+        $this->controller = $collection->getController();
+        parent::__construct($collection, $settings);
+    }
+
+    public function initialize($controller) {
+        
+    }
+
+    public function startup($controller) {
+        
+    }
+
+    public function beforeRender($controller) {
+        
+    }
+
+    public function shutDown($controller) {
+        
+    }
+
+    public function beforeRedirect($controller, $url, $status = null, $exit = true) {
+        
+    }
+
+}
