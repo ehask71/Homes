3f2a92db4dea7fc97fe6eb9f0551c12f20982712
ehask71 <ehask71@gmail.com>
2013-10-05 00:38:27 -0400

    Registration Flow

--- app/Controller/AccountController.php
+++ app/Controller/AccountController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP AccountController
  * @author Eric
@@ -6,80 +7,50 @@
 App::uses('AppController', 'Controller');
 
 class AccountController extends AppController {
-    
+
     public $name = 'Account';
     public $uses = array('Account');
     public $components = array('AuthNetXml');
 
-
     public function beforeFilter() {
 	parent::beforeFilter();
-        $this->Auth->allow('register','login','logout','forgotpwd');
+	$this->Auth->allow('register', 'login', 'logout', 'forgotpwd');
     }
-    
-    public function index(){
-        $this->autoRender = false;
-        $account = $this->Account->find('first',array(
-            'conditions' => array(
-                'Account.email' => 'ehask71@gmail.com'
-            )
-        ));
-        echo '<pre>';
-        print_r($account);
+
+    public function index() {
+	$this->autoRender = false;
+	$account = $this->Account->find('first', array(
+	    'conditions' => array(
+		'Account.email' => 'ehask71@gmail.com'
+	    )
+		));
+	echo '<pre>';
+	print_r($account);
     }
-    
-    public function register(){
-        if ($this->request->is('post')) {
+
+    public function register() {
+	if ($this->request->is('post')) {
 	    $this->Account->set($this->data);
 	    if ($this->Account->accountValidate()) {
 		if ($this->Account->save($this->request->data)) {
 		    $userid = $this->Account->getLastInsertID();
-                    // Create Profile on Auth.Net
-                    $data['id'] = $userid;
-                    $data['email'] = $this->request->data['Account']['email'];
-                    $data['firstname'] = $this->request->data['Account']['firstname'];
-                    $data['lastname'] = $this->request->data['Account']['lastname'];
-                    $data['address'] = $this->request->data['Account']['address'];
-                    $data['city'] = $this->request->data['Account']['city'];
-                    $data['state'] = $this->request->data['Account']['state'];
-                    $data['zip'] = $this->request->data['Account']['zip'];
-                    $data['phone'] = $this->request->data['Account']['phone'];
-                    $data['ccnum'] = '4111111111111111';
-                    $data['ccexpyr'] = '2014';
-                    $data['ccexpmnth'] = '11';
-                    $data['cvv'] = '123';
-                    
-		    $cimresponse = $this->AuthNetXml->customer_profile_request($data);
-                    if(!$cimresponse->isError()){
-                        CakeLog::write('debug','CIM success');
-                        $update['Account']['id'] = $userid;
-                        $update['Account']['authnet_profile'] = $cimresponse->customerProfileId;
-                        $this->Account->create();
-                        $this->Account->save($update);
-                    } else {
-                        //echo $cimresponse->__toString();
-                        CakeLog::write('debug', $cimresponse->messages->resultCode.' '.$cimresponse->messages->message->code.' '.$cimresponse->customerProfileId.' '.$cimresponse->customerPaymentProfileIdList->numericString);
-                    }
-                    //echo $cimresponse->__toString();
-                    CakeLog::write('debug', $cimresponse->messages->resultCode.' '.$cimresponse->messages->message->code.' '.$cimresponse->customerProfileId.' '.$cimresponse->customerPaymentProfileIdList->numericString);
 		    // Assign a Role
 		    $this->loadModel('RoleUser');
 		    $roleuser = $this->RoleUser->addUserSite($userid);
 		    // Log the user in
-		    //$id = $this->Account->id;
 		    $role = array();
 		    $role[] = array(
-			'id'=>2,
-			'alias'=>'member',
+			'id' => 2,
+			'alias' => 'member',
 			'RolesUser' => array(
 			    'id' => $roleuser,
 			    'user_id' => $userid,
 			    'role_id' => 2
 			)
 		    );
-		    $this->request->data['Account'] = array_merge($this->request->data['Account'], array('id' => $userid,'Role'=>$role));
+		    $this->request->data['Account'] = array_merge($this->request->data['Account'], array('id' => $userid, 'Role' => $role));
 		    $this->Auth->login($this->request->data['Account']);
-		   
+
 		    $this->Session->setFlash('The user has been saved');
 		    $this->redirect('/register/select-counties');
 		} else {
@@ -87,41 +58,67 @@ class AccountController extends AppController {
 		}
 	    }
 	}
-	//$this->set('countries', $this->Country->getCountries());
     }
-    
+
     // Registration Step 2
-    public function selectcounties(){
+    public function selectcounties() {
 	
     }
-    
+
     // Billing Profile
-    public function billingprofile(){
+    public function billingprofile() {
+	if ($this->request->is('post')) {
+	    // Billing Profile
+	    $data['id'] = $userid;
+	    $data['email'] = $this->request->data['Account']['email'];
+	    $data['firstname'] = $this->request->data['Account']['firstname'];
+	    $data['lastname'] = $this->request->data['Account']['lastname'];
+	    $data['address'] = $this->request->data['Account']['address'];
+	    $data['city'] = $this->request->data['Account']['city'];
+	    $data['state'] = $this->request->data['Account']['state'];
+	    $data['zip'] = $this->request->data['Account']['zip'];
+	    $data['phone'] = $this->request->data['Account']['phone'];
+	    $data['ccnum'] = '4111111111111111';
+	    $data['ccexpyr'] = '2014';
+	    $data['ccexpmnth'] = '11';
+	    $data['cvv'] = '123';
+
+	    $cimresponse = $this->AuthNetXml->customer_profile_request($data);
+	    if (!$cimresponse->isError()) {
+		CakeLog::write('debug', 'CIM success');
+		$update['Account']['id'] = $userid;
+		$update['Account']['authnet_profile'] = $cimresponse->customerProfileId;
+		$this->Account->create();
+		$this->Account->save($update);
+	    } else {
+		//echo $cimresponse->__toString();
+		CakeLog::write('debug', $cimresponse->messages->resultCode . ' ' . $cimresponse->messages->message->code . ' ' . $cimresponse->customerProfileId . ' ' . $cimresponse->customerPaymentProfileIdList->numericString);
+	    }
+	}
+    }
+
+    public function finshregistration() {
 	
     }
-    
-    public function finshregistration(){
-	
-    }
-    
+
     public function login() {
 	if ($this->request->is('post')) {
 	    if ($this->Auth->login()) {
 		$this->redirect($this->Auth->redirect());
 	    } else {
-                echo '<pre>';
-                print_r($this->Auth->user());
-                //print_r($this->Auth);
-                echo '</pre>';
+		echo '<pre>';
+		print_r($this->Auth->user());
+		//print_r($this->Auth);
+		echo '</pre>';
 		$this->Session->setFlash('Your username or password was incorrect.');
 	    }
 	}
     }
-    
+
     public function logout() {
-        $this->redirect($this->Auth->logout());
+	$this->redirect($this->Auth->logout());
     }
-    
+
     public function forgetpwd() {
 	if ($this->request->is('post')) {
 	    $account = $this->Account->find('first', array(
@@ -221,7 +218,7 @@ class AccountController extends AppController {
 	    $this->render('entercode');
 	}
     }
-    
+
     public function isAuthorized($user) {
 	if ($user['role'] == 'admin') {
 	    return true;
@@ -233,5 +230,6 @@ class AccountController extends AppController {
 	}
 	return true;
     }
+
 }
 
