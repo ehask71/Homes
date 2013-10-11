eb8803f47cb9d8e751564421b575625800cf2313
Eric Haskins <ehask71@gmail.com>
2013-10-11 15:03:43 -0400

    pdf

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -51,7 +51,7 @@
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
-
+	Router::parseExtensions();
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
