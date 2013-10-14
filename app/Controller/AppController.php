5d8efc6e334b289b5049f8aa8a3b473530853b95
Eric Haskins <ehask71@gmail.com>
2013-10-14 12:21:56 -0400

    pdf

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -2,35 +2,48 @@
 
 App::uses('Controller', 'Controller');
 
-
 class AppController extends Controller {
-    
+
     public $viewClass = 'Theme';
     public $theme = 'default';
-    
+    public $helpers = array(
+	'Session',
+	'Html' => array('className' => 'BoostCake.BoostCakeHtml'),
+	'Form' => array('className' => 'BoostCake.BoostCakeForm'),
+	'Paginator' => array('className' => 'BoostCake.BoostCakePaginator'),
+    );
     public $components = array(
-        'Session',
+	'Session',
 	'RequestHandler',
-        'Auth' => array(
-            'authorize' => array('Tiny'),
-            'authenticate' => array(
-                'all' => array('userModel' => 'Account'),
-                'Form' => array(
-                    'fields' => array('username' => 'email', 'password' => 'password'),
-                    'scope' => array(
-                        'Account.is_active' => 'yes'
-                    ),
-                    'recursive' => 1,
-            )),
-            'loginRedirect' => array('controller' => 'home', 'action' => 'index'),
-            'logoutRedirect' => array('controller' => 'account', 'action' => 'login'),
-            'loginAction' => '/login',
-        ),
+	'Auth' => array(
+	    'authorize' => array('Tiny'),
+	    'authenticate' => array(
+		'all' => array('userModel' => 'Account'),
+		'Form' => array(
+		    'fields' => array('username' => 'email', 'password' => 'password'),
+		    'scope' => array(
+			'Account.is_active' => 'yes'
+		    ),
+		    'recursive' => 1,
+		)),
+	    'loginRedirect' => array('controller' => 'home', 'action' => 'index'),
+	    'logoutRedirect' => array('controller' => 'account', 'action' => 'login'),
+	    'loginAction' => '/login',
+	    'flash' => array(
+		'element' => 'alert',
+		'key' => 'auth',
+		'params' => array(
+		    'plugin' => 'BoostCake',
+		    'class' => 'alert-error'
+		)
+	    )
+	),
 	'DebugKit.Toolbar'
-	    );
-    
+    );
+
     public function beforeFilter() {
 	$this->set('userinfo', $this->Auth->user());
-        $this->set('loggedIn', $this->Auth->loggedIn());
+	$this->set('loggedIn', $this->Auth->loggedIn());
     }
+
 }
