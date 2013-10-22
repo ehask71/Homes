097e9634e92d5d63746942e13901c606fade79b2
ehask71 <ehask71@gmail.com>
2013-10-22 17:27:10 -0400

    Added Profile Functions

--- app/Controller/AccountController.php
+++ app/Controller/AccountController.php
@@ -67,6 +67,7 @@ class AccountController extends AppController {
 			'expiration' => $cimresponse->profile->paymentProfiles->payment->creditCard->expirationDate
 		    )
 		);
+		$profile = json_decode(json_encode((array) simplexml_load_string($cimresponse->profile)), 1);
 		$this->set('profile',$profile);
 	    } else {
 		$this->Session->setFlash(__('No Billing Profiles Found!'));
