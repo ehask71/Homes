71cd2d01d0ba580e6dde104a6d33318dc6325dc1
EricMain <EricMain@EricMain-PC.home>
2013-10-22 21:45:55 -0400

    Initial Framework

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -110,6 +110,7 @@ CakeLog::config('error', array(
 /**
  *   Config Section
  */
+Configure::write('Copyright','HO Brands 2013');
 Configure::write('SitePrefix','CFH');
 Configure::write('Authnet.apilogin', '6VckCd8562vT');
 Configure::write('Authnet.txnkey', '22Fk7S5srmm7B57r');
