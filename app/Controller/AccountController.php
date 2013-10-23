858e0db1b055c95a64113a7db0a15c16bbbb9b11
EricMain <EricMain@EricMain-PC.home>
2013-10-22 21:00:50 -0400

    Initial Framework

--- app/Controller/AccountController.php
+++ app/Controller/AccountController.php
@@ -52,7 +52,7 @@ class AccountController extends AppController {
 	    $cimresponse = $this->AuthNetXml->get_customer_profile($data);
 	    if (!$cimresponse->isError()) {
                 //echo $cimresponse->customerPaymentProfileId;
-		$profile = (array)$cimresponse->__get('profile');
+		$profile = $cimresponse->__get('profile');
 		
 		$this->set('profile',$profile);
 	    } else {
