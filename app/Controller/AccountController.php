9ed69fab1a3efbcc13c27909af3838769b10e8a6
EricMain <EricMain@EricMain-PC.home>
2013-10-22 20:17:49 -0400

    Initial Framework

--- app/Controller/AccountController.php
+++ app/Controller/AccountController.php
@@ -73,7 +73,7 @@ class AccountController extends AppController {
 		}
                 print_r($xml);
                 var_dump($xml);
-                print_r($cimresponse);
+                print_r($cimresponse->response_xml);
                 echo "</pre>";
 		
 		$this->set('profile',$profile);
