a43ad77a7ce11b887bf6a575c353e0f708a887cc
ehask71 <ehask71@gmail.com>
2013-10-23 00:15:18 -0400

    Added Profile Functions

--- app/Config/core.php
+++ app/Config/core.php
@@ -151,7 +151,7 @@
  *	`manager_index()` and `/manager/controller/index`
  *
  */
-	Configure::write('Routing.prefixes', array('admin'));
+	Configure::write('Routing.prefixes', array('admin','professionals'));
 
 /**
  * Turn off all caching application-wide.
