d9604292375633fd6ca51bdeb1becf09c134f463
EricMain <EricMain@EricMain-PC.home>
2013-11-07 02:29:23 -0500

    New Attachment Model

---
+++ app/Model/Attachment.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * CakePHP Attachment
+ * @author EricMain
+ */
+App::uses('Model', 'Model');
+
+class Attachment extends AppModel {
+    
+    public $name = 'Attachment';
+    public $primaryKey = 'id';
+   
+
+}
