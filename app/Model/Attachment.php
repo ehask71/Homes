bc29e514dc17ac3b2296adaa2274faf9675829ad
EricMain <EricMain@EricMain-PC.home>
2013-11-07 02:33:49 -0500

    New Attachment Model

--- app/Model/Attachment.php
+++ app/Model/Attachment.php
@@ -10,6 +10,27 @@ class Attachment extends AppModel {
     
     public $name = 'Attachment';
     public $primaryKey = 'id';
+    
+    public $actsAs = array(
+        'Upload.Upload' => array(
+            'attachment' => array(
+                'thumbnailSizes' => array(
+                    'xvga' => '1024x768',
+                    'vga' => '640x480',
+                    'thumb' => '80x80',
+                ),
+            ),
+        ),
+    );
    
-
+    public $belongsTo = array(
+        'Post' => array(
+            'className' => 'Post',
+            'foreignKey' => 'foreign_key',
+        ),
+        'Message' => array(
+            'className' => 'Message',
+            'foreignKey' => 'foreign_key',
+        ),
+    );
 }
