06d9bf422797e762ada0393c39b2982f381bb863
EricMain <EricMain@EricMain-PC.home>
2013-11-08 23:34:59 -0500

    New Attachment Model

--- app/Controller/UploadController.php
+++ app/Controller/UploadController.php
@@ -1,25 +1,26 @@
 <?php
+
 App::uses('AppController', 'Controller');
+
 /**
  * CakePHP UploadController
  * @author EricMain
  */
-
 class UploadController extends AppController {
 
     public $name = 'UploadController';
-    
     public $components = array();
-    
     public $uses = array();
 
-
     public function beforeFilter() {
+        if (isset($this->params['session_id'])) {
+            $this->Session->id($this->params['session_id']);
+        }
         parent::beforeFilter();
         $this->Auth->allow();
     }
-    
-    public function index(){
+
+    public function index() {
         
     }
 
