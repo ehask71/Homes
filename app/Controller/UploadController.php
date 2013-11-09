5e407d8a19a6761f26827d35988074be3b2a1e5b
EricMain <EricMain@EricMain-PC.home>
2013-11-09 11:06:21 -0500

    Pathing issues

--- app/Controller/UploadController.php
+++ app/Controller/UploadController.php
@@ -24,14 +24,14 @@ class UploadController extends AppController {
         $this->autoRender = false;
         $verifyToken = md5('cashfor2013HO' . $_POST['timestamp']);
         if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
-            $tempFile = $_FILES['Filedata']['tmp_name'];
-            $targetPath = ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS . 'files';
-            $targetFile = rtrim($targetPath, DS) . DS . $_FILES['Filedata']['name'];
-
             // Validate the file type
             $fileTypes = array('jpg', 'jpeg', 'gif', 'png'); // File extensions
             $fileParts = pathinfo($_FILES['Filedata']['name']);
-
+            // Rename 
+            $tempFile = $_FILES['Filedata']['tmp_name'];
+            $targetPath = ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS . 'files';
+            $targetFile = rtrim($targetPath, DS) . DS . $_FILES['Filedata']['name'];
+            echo $_FILES['Filedata']['name'];
             if (in_array($fileParts['extension'], $fileTypes)) {
                 move_uploaded_file($tempFile, $targetFile);
                 echo '1';
