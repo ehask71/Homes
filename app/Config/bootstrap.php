4624feb4a819f12f925ad64359b2e82b45c3c273
EricMain <EricMain@EricMain-PC.home>
2013-11-09 10:25:12 -0500

    added scriptBottom block

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -71,7 +71,7 @@ Cache::config('default', array('engine' => 'File'));
  */
 CakePlugin::load('DebugKit');
 CakePlugin::load('BoostCake');
-CakePlugin::load('Upload');
+//CakePlugin::load('Upload');
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter . By Default CakePHP bundles two filters:
  *
