01c0d12bdbfe534563543c64969ad9d833f21ed6
Eric Haskins <ehask71@gmail.com>
2013-11-25 16:43:59 -0500

    removing debug code

--- app/Model/Tmplead.php
+++ app/Model/Tmplead.php
@@ -1,17 +1,11 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  * CakePHP Tmplead
  * @author EricMain
  */
 App::uses('Model', 'Model');
 
-class TmpleadModel extends Model {
+class Tmplead extends Model {
     
     public $name = 'Tmplead';
     public $primaryKey = 'id';
