345029596377d820c04d6458aae37570a84deaf7
Eric Haskins <ehask71@gmail.com>
2013-11-27 15:28:40 -0500

    Ticket Lookup

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -8,6 +8,7 @@ class AppController extends Controller {
     public $theme = 'cashfor';
     public $helpers = array(
 	'Session',
+        'Auth',
 	'Html' => array('className' => 'BoostCake.BoostCakeHtml'),
 	'Form' => array('className' => 'BoostCake.BoostCakeForm'),
 	'Paginator' => array('className' => 'BoostCake.BoostCakePaginator'),
