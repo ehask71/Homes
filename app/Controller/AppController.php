e218e4c6944a8bc4c13cac84737db8786055896e
Eric Haskins <ehask71@gmail.com>
2013-11-27 15:36:45 -0500

    Ticket Lookup

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -44,7 +44,7 @@ class AppController extends Controller {
 
     public function beforeFilter() {
 	if (isset($this->params['prefix']) && $this->params['prefix'] == 'admin') {
-	    $this->layout = 'admin';
+	    $this->theme = 'admin';
 	}
 	if(isset($this->params['prefix']) && $this->params['prefix'] == 'professionals'){
 	    $this->layout = 'professional';
