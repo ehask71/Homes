de974a56a29b3580990eb14434c7edfcc3edbf20
Eric Haskins <ehask71@gmail.com>
2013-12-11 23:22:19 -0500

    getProfileIds Auth.net

--- app/Controller/Component/AuthNetXmlComponent.php
+++ app/Controller/Component/AuthNetXmlComponent.php
@@ -10,7 +10,7 @@ class AuthNetXmlComponent extends Component {
 
     public $components = array();
     public $xml = null;
-    public $validationMode = 'testMode';//'liveMode';
+    public $validationMode = 'testMode'; //'liveMode';
     private $server = AuthnetXML::USE_DEVELOPMENT_SERVER;
 
     public function __construct(\ComponentCollection $collection, $settings = array()) {
@@ -62,35 +62,35 @@ class AuthNetXmlComponent extends Component {
         $xml->createCustomerProfileRequest(array(
             'profile' => array(
                 'merchantCustomerId' => Configure::read('SitePrefix') . $data['id'],
-                'email' => $data['email']
-            ),
-            'paymentProfiles' => array(
-                'billTo' => array(
-                    'firstName' => $data['firstname'],
-                    'lastName' => $data['lastname'],
-                    'address' => $data['address'],
-                    'city' => $data['city'],
-                    'state' => $data['state'],
-                    'zip' => $data['zip'],
-                    'phoneNumber' => $data['phone']
-                ),
-                'payment' => array(
-                    'creditCard' => array(
-                        'cardNumber' => $data['ccnum'],
-                        'expirationDate' => $data['ccexpyr'] . '-' . $data['ccexpmnth'],
-                        'cardCode' => $data['cvv']
+                'email' => $data['email'],
+                'paymentProfiles' => array(
+                    'billTo' => array(
+                        'firstName' => $data['firstname'],
+                        'lastName' => $data['lastname'],
+                        'address' => $data['address'],
+                        'city' => $data['city'],
+                        'state' => $data['state'],
+                        'zip' => $data['zip'],
+                        'phoneNumber' => $data['phone']
                     ),
-                ),
+                    'payment' => array(
+                        'creditCard' => array(
+                            'cardNumber' => $data['ccnum'],
+                            'expirationDate' => $data['ccexpyr'] . '-' . $data['ccexpmnth'],
+                            'cardCode' => $data['cvv']
+                        ),
+                    ),
+                )
             ),
             'validationMode' => $this->validationMode
         ));
         if (!$xml->isError()) {
-            return array('profileId'=>$xml->customerProfileId,'paymentId'=>(array)$xml->customerPaymentProfileIdList->numericString);
+            return array('profileId' => $xml->customerProfileId, 'paymentId' => (array) $xml->customerPaymentProfileIdList->numericString);
         }
         CakeLog::write('debug', $xml->messages->resultCode . ' ' . $xml->messages->message->code . ' ' . $xml->customerProfileId . ' ' . $xml->customerPaymentProfileIdList->numericString);
         return false;
     }
-    
+
     public function create_billing_profile($data) {
         $xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
         $xml->createCustomerProfileRequest(array(
@@ -114,7 +114,7 @@ class AuthNetXmlComponent extends Component {
             ),
             'validationMode' => 'testMode'
         ));
-        
+
         return $xml;
     }
 
@@ -133,6 +133,5 @@ class AuthNetXmlComponent extends Component {
 
         return $xml;
     }
-    
 
 }
