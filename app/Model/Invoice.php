cdb4bc840a3ca7379a58b0064e1035c2b7166d12
ehaskins <ehaskins@192.168.0.3>
2014-02-04 23:55:48 -0500

    campaign view file

--- app/Model/Invoice.php
+++ app/Model/Invoice.php
@@ -1,12 +1,33 @@
 <?php
+
 /**
  * CakePHP Invoice
  * @author ehaskins
  */
 App::uses('AppModel', 'Model');
+App::uses('Order', 'Model');
+App::uses('InvoiceItem', 'Model');
 
 class Invoice extends AppModel {
-    public $primaryKey = 'id';    
-    
-}
 
+    public $primaryKey = 'id';
+
+    public function createInvoice($ordid) {
+        $order = new Order();
+        $orderinfo = $order->find('first', array(
+            'conditions' => array(
+                'Order.id' => $ordid
+            )
+          )
+        );
+
+        $data[$this->alias]['account_id'] = $order->field('account_id');
+        $data[$this->alias]['order_id'] = $order->field('order_id');
+        $data[$this->alias]['name'] = '';
+        $data[$this->alias]['desc'] = '';
+        $data[$this->alias]['total'] = '';
+        $data[$this->alias]['paid'] = 0;
+        $this->saveAll($data);
+    }
+
+}
