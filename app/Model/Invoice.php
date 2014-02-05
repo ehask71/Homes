1c21042994e929e342b1be12d2050a093e50a4ce
ehaskins <ehaskins@192.168.0.3>
2014-02-05 18:58:41 -0500

    campaign view file

--- app/Model/Invoice.php
+++ app/Model/Invoice.php
@@ -12,7 +12,7 @@ class Invoice extends AppModel {
 
     public $primaryKey = 'id';
 
-    public function createInvoice($ordid) {
+    public function createInvoice($ordid,$setup=FALSE) {
         $order = new Order();
         $orderinfo = $order->find('first', array(
             'conditions' => array(
@@ -20,7 +20,7 @@ class Invoice extends AppModel {
             )
           )
         );
-
+        $data = array();
         $data[$this->alias]['account_id'] = $orderinfo[$this->alias]['account_id'];
         $data[$this->alias]['order_id'] = $orderinfo[$this->alias]['order_id'];
         $data[$this->alias]['name'] = '';
@@ -28,8 +28,15 @@ class Invoice extends AppModel {
         $data[$this->alias]['total'] = '';
         $data[$this->alias]['paid'] = 0;
         // Add Items
+        $i = 0;
         foreach($orderinfo['OrderItem'] AS $item){
-            
+            $data[$i]['InvoiceItem']['description'] = $item['name'];
+            $data[$i]['InvoiceItem']['price'] = $item['price'];
+            $i++;  
+        }
+        if($setup){
+            $data[$i]['InvoiceItem']['description'] = 'Setup Fee';
+            $data[$i]['InvoiceItem']['price'] = sprintf('%0.2f', (int) Configure::read('Setupfee'));
         }
         $this->saveAll($data);
     }
