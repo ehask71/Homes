36b93f9b348735b316e3982ae96a9965e718ee4a
Eric Haskins <ehask71@gmail.com>
2014-02-06 08:48:44 -0500

    Merge origin/master
    Conflicts:
    	app/Controller/RegistrationController.php

--- app/Model/InvoiceItem.php
+++ app/Model/InvoiceItem.php
@@ -1,14 +1,14 @@
 <?php
+
 /**
  * CakePHP InvoiceItem
  * @author ehaskins
  */
 App::uses('AppModel', 'Model');
 
-class InvoiceItem  extends AppModel {
-    public $primaryKey = 'id';   
-    
-    public $belongsTo = array('Invoice');
-    
-}
+class InvoiceItem extends AppModel {
 
+    public $primaryKey = 'id';
+    public $belongsTo = array('Invoice');
+
+}
