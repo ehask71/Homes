7a1a3dc12d4abfe29ec65d22df06ada97b3eee47
Eric Haskins <ehask71@gmail.com>
2014-02-06 09:32:13 -0500

    added , array('validate' => 'first')  to create function saveAll

--- app/Controller/Component/AuthNetXmlComponent.php
+++ app/Controller/Component/AuthNetXmlComponent.php
@@ -14,130 +14,131 @@ class AuthNetXmlComponent extends Component {
     private $server = AuthnetXML::USE_DEVELOPMENT_SERVER;
 
     public function __construct(\ComponentCollection $collection, $settings = array()) {
-        $this->controller = $collection->getController();
-        parent::__construct($collection, $settings);
+	$this->controller = $collection->getController();
+	parent::__construct($collection, $settings);
     }
 
     public function initialize(Controller $controller) {
-        
+	
     }
 
     public function startup(Controller $controller) {
-        
+	
     }
 
     public function payment_profile_create($profileid, $data) {
-        
+	
     }
 
     public function create_customer_profile_request($data) {
-        $xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
-        $xml->createCustomerProfileRequest(array(
-            'profile' => array(
-                'merchantCustomerId' => Configure::read('SitePrefix') . $data['id'],
-                'email' => $data['email']
-            ),
-            'validationMode' => $this->validationMode
-        ));
-        if (!$xml->isError()) {
-            return $xml->customerProfileId;
-        }
-        return false;
+	$xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
+	$xml->createCustomerProfileRequest(array(
+	    'profile' => array(
+		'merchantCustomerId' => Configure::read('SitePrefix') . $data['id'],
+		'email' => $data['email']
+	    ),
+	    'validationMode' => $this->validationMode
+	));
+	if (!$xml->isError()) {
+	    return $xml->customerProfileId;
+	}
+	return false;
     }
 
     public function create_customer_full_profile_request($data) {
-        $xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
-        $xml->createCustomerProfileRequest(array(
-            'profile' => array(
-                'merchantCustomerId' => Configure::read('SitePrefix') . $data['id'],
-                'email' => $data['email'],
-                'paymentProfiles' => array(
-                    'billTo' => array(
-                        'firstName' => $data['firstname'],
-                        'lastName' => $data['lastname'],
-                        'address' => $data['address'],
-                        'city' => $data['city'],
-                        'state' => $data['state'],
-                        'zip' => $data['zip'],
-                        'phoneNumber' => $data['phone']
-                    ),
-                    'payment' => array(
-                        'creditCard' => array(
-                            'cardNumber' => $data['ccnum'],
-                            'expirationDate' => $data['ccexpyr'] . '-' . $data['ccexpmnth'],
-                            'cardCode' => $data['cvv']
-                        ),
-                    ),
-                )
-            ),
-            'validationMode' => $this->validationMode
-        ));
-        if (!$xml->isError()) {
-            //CakeLog::write('debug', $xml->messages->resultCode . ' ' . $xml->messages->message->code . ' ' . $xml->customerProfileId . ' ' . $xml->customerPaymentProfileIdList->numericString);
-            return array('profileId' => $xml->customerProfileId, 'paymentId' => $xml->customerPaymentProfileIdList->numericString);
-        }
-        CakeLog::write('debug', $xml->messages->resultCode . ' ' . $xml->messages->message->code . ' ' . $xml->customerProfileId . ' ' . $xml->customerPaymentProfileIdList->numericString);
-        return false;
+	$xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
+	$xml->createCustomerProfileRequest(array(
+	    'profile' => array(
+		'merchantCustomerId' => Configure::read('SitePrefix') . $data['id'],
+		'email' => $data['email'],
+		'paymentProfiles' => array(
+		    'billTo' => array(
+			'firstName' => $data['firstname'],
+			'lastName' => $data['lastname'],
+			'address' => $data['address'],
+			'city' => $data['city'],
+			'state' => $data['state'],
+			'zip' => $data['zip'],
+			'phoneNumber' => $data['phone']
+		    ),
+		    'payment' => array(
+			'creditCard' => array(
+			    'cardNumber' => $data['ccnum'],
+			    'expirationDate' => $data['ccexpyr'] . '-' . $data['ccexpmnth'],
+			    'cardCode' => $data['cvv']
+			),
+		    ),
+		)
+	    ),
+	    'validationMode' => $this->validationMode
+	));
+	if (!$xml->isError()) {
+	    //CakeLog::write('debug', $xml->messages->resultCode . ' ' . $xml->messages->message->code . ' ' . $xml->customerProfileId . ' ' . $xml->customerPaymentProfileIdList->numericString);
+	    return array('profileId' => $xml->customerProfileId, 'paymentId' => $xml->customerPaymentProfileIdList->numericString);
+	}
+	CakeLog::write('debug', $xml->messages->resultCode . ' ' . $xml->messages->message->code . ' ' . $xml->customerProfileId . ' ' . $xml->customerPaymentProfileIdList->numericString);
+	return false;
     }
 
     public function create_billing_profile($data) {
-        $xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
-        $xml->createCustomerProfileRequest(array(
-            'paymentProfiles' => array(
-                'billTo' => array(
-                    'firstName' => $data['firstname'],
-                    'lastName' => $data['lastname'],
-                    'address' => $data['address'],
-                    'city' => $data['city'],
-                    'state' => $data['state'],
-                    'zip' => $data['zip'],
-                    'phoneNumber' => $data['phone']
-                ),
-                'payment' => array(
-                    'creditCard' => array(
-                        'cardNumber' => $data['ccnum'],
-                        'expirationDate' => $data['ccexpyr'] . '-' . $data['ccexpmnth'],
-                        'cardCode' => $data['cvv']
-                    ),
-                ),
-            ),
-            'validationMode' => 'testMode'
-        ));
+	$xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
+	$xml->createCustomerProfileRequest(array(
+	    'paymentProfiles' => array(
+		'billTo' => array(
+		    'firstName' => $data['firstname'],
+		    'lastName' => $data['lastname'],
+		    'address' => $data['address'],
+		    'city' => $data['city'],
+		    'state' => $data['state'],
+		    'zip' => $data['zip'],
+		    'phoneNumber' => $data['phone']
+		),
+		'payment' => array(
+		    'creditCard' => array(
+			'cardNumber' => $data['ccnum'],
+			'expirationDate' => $data['ccexpyr'] . '-' . $data['ccexpmnth'],
+			'cardCode' => $data['cvv']
+		    ),
+		),
+	    ),
+	    'validationMode' => 'testMode'
+	));
 
-        return $xml;
+	return $xml;
     }
-    
-    public function createCustomerProfileTransactionRequest($data){
-        $xml = new AuthNetXml(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
-        $xml->createCustomerProfileTransactionRequest(array(
-            'profileTransAuthCapture' => array(
-                'amount' => $data['amount'],
-                'customerProfileId' => $data['authnet_profile'],
-                'customerPaymentProfileId' => $data['authnet_payment'],
-                'order' => array(
-                    'invoiceNumber' => $data['invoice'],
-                    'description' => @$data['description']
-                )
-            ),
-        ));
-        
-        return $xml;
+
+    public function createCustomerProfileTransactionRequest($data) {
+	$xml = new AuthNetXml(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
+	$xml->createCustomerProfileTransactionRequest(array(
+	    'transaction' => array(
+		'profileTransAuthCapture' => array(
+		    'amount' => $data['amount'],
+		    'customerProfileId' => $data['authnet_profile'],
+		    'customerPaymentProfileId' => $data['authnet_payment'],
+		    'order' => array(
+			'invoiceNumber' => $data['invoice'],
+			'description' => @$data['description']
+		    )
+		),
+	)));
+
+	return $xml;
     }
 
     public function get_customer_profile($data) {
-        $xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
-        $xml->getCustomerProfileRequest(array(
-            'customerProfileId' => $data['customerProfileId']
-        ));
+	$xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
+	$xml->getCustomerProfileRequest(array(
+	    'customerProfileId' => $data['customerProfileId']
+	));
 
-        return $xml;
+	return $xml;
     }
 
     public function get_customer_profile_ids($data) {
-        $xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
-        $xml->getCustomerProfileIdsRequest(array());
+	$xml = new AuthnetXML(Configure::read('Authnet.apilogin'), Configure::read('Authnet.txnkey'), $this->server);
+	$xml->getCustomerProfileIdsRequest(array());
 
-        return $xml;
+	return $xml;
     }
 
 }
