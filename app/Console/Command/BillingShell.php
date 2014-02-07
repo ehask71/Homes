96a42761fd001378ef62612258ebecb59616e83b
Eric Haskins <ehask71@gmail.com>
2014-02-07 06:45:21 -0500

    adding tasks

--- app/Console/Command/BillingShell.php
+++ app/Console/Command/BillingShell.php
@@ -6,13 +6,12 @@ class BillingShell extends AppShell {
 
     public function main() {
         // Notify Customers 
-        echo "Starting Billing Cron.....\n";
+        $this->out("Starting Billing Cron.....");
         mail('ehask71@gmail.com','Test', 'Testing Cron '.$this->args[1]);
         if($this->args[0] == 'notify'){
             // Run Summary  cron ->  cd /home/ehask/public_html/vverify/app && Console/cake site summary 15
-            echo 'Running Notify Interval '.$this->args[1]."\n";
+            $this->out("Running Notify Interval ".$this->args[1]);
             //$this->Notify->execute($this->args[1]);
-
         }
         if($this->args[0] == 'generateInvoices'){
             
