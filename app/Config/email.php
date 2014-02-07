6140f8c40ec70c2b5a70a4bf5a4f06b761a8a783
Eric Haskins <ehask71@gmail.com>
2014-02-07 07:17:01 -0500

    adding tasks

--- app/Config/email.php
+++ app/Config/email.php
@@ -8,7 +8,7 @@
 
 class EmailConfig {
 
-	public $default = array(
+	/*public $default = array(
 		'transport' => 'Smtp',
 		'from' => array('do-not-reply@cashforhomes.com' => 'CashForHomes'),
 		'host' => 'localhost',
@@ -18,6 +18,12 @@ class EmailConfig {
 		'password' => 'secret',
 		'client' => null,
 		'log' => false,
+		//'charset' => 'utf-8',
+		//'headerCharset' => 'utf-8',
+	);*/
+	public $default = array(
+		'transport' => 'Mail',
+		'from' => 'do-not-reply@cashforhomes.com',
 		//'charset' => 'utf-8',
 		//'headerCharset' => 'utf-8',
 	);
