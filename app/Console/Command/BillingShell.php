94a1b47231c28f447993878f62c28ad1d2c837e7
ehaskins <ehaskins@192.168.0.3>
2014-02-06 23:03:35 -0500

    Billing Shell

--- app/Console/Command/BillingShell.php
+++ app/Console/Command/BillingShell.php
@@ -2,15 +2,19 @@
 
 class BillingShell extends AppShell {
 
-    //public $tasks = array('Summarize');
+    public $tasks = array('Notify');
 
     public function main() {
-        if($this->args[0] == 'summary'){
+        // Notify Customers 
+        if($this->args[0] == 'notify'){
             // Run Summary  cron ->  cd /home/ehask/public_html/vverify/app && Console/cake site summary 15
-            $this->Summarize->execute($this->args[1]);
-	    // WTF
-	    //mail('ehask71@gmail.com','Summarize','Summary Completed @ '.date('Y m d H:i:s'));
+            $this->Notify->execute($this->args[1]);
+
         }
+        if($this->args[0] == 'generateInvoices'){
+            
+        }
+        
     }
 
 }
