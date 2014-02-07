0acbac807d16287ed6f3110623e53d449152d1b8
ehaskins <ehaskins@192.168.0.3>
2014-02-06 22:24:10 -0500

    Add Notify Task

---
+++ app/Console/Command/Task/NotifyTask.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * Copyright 2014 High Octane Brands LLC
+ * You may not use this file unless you or your domain are properly
+ * Licensed to do so.
+ */
+
+class Notify extends Shell {
+
+    //public $uses = array('RawData', 'Campaign');
+
+    public function execute() {
+
+	
+	return true;
+    }
+
+}
+
