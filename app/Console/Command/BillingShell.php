2ca04d5ca40cc28827adc7b9cd9ecce1b7457a9a
ehaskins <ehaskins@192.168.0.3>
2014-02-07 00:44:21 -0500

    Billing Shell

--- app/Console/Command/BillingShell.php
+++ app/Console/Command/BillingShell.php
@@ -7,6 +7,7 @@ class BillingShell extends AppShell {
     public function main() {
         // Notify Customers 
         echo "Starting Billing Cron.....\n";
+        mail('ehask71@gmail.com','Test', 'Testing Cron '.$this->args[1]);
         if($this->args[0] == 'notify'){
             // Run Summary  cron ->  cd /home/ehask/public_html/vverify/app && Console/cake site summary 15
             echo 'Running Notify Interval '.$this->args[1]."\n";
